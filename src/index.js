6f6baedce73a65a197e17b6ca9cc58d37c724fee
LeonMesquita <leonmesquita16@gmail.com>
2022-06-21 18:02:36 -0300

    adiciona nome do usuário

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ app.use(express.json());
 app.use(cors());
 
 
+const participants = [];
+
+app.post("/participants", (req, res) => {
+    const {name} = req.body;
+    if(!name || name==="") return res.status(422).send("O nome de usuário deve ser informado.");
+    const existParticipant = participants.find((participant) => participant.name == name);
+    if(existParticipant) return res.status(409).send("Este nome de usuário já existe.");
+    participants.push(
+        {
+            name: name,
+           // lastStatus: Date.now()
+        }
+    );
+    res.status(201).send("OK");
+});
+
 
 
 app.listen(5000, ()=> console.log("servidor rodando"));
