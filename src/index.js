165ce302d62aca780376691c5447279cfc4b0927
LeonMesquita <leonmesquita16@gmail.com>
2022-06-22 23:29:36 -0300

    criação do banco de dados

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import {MongoClient} from 'mongodb';
+
+const client = new MongoClient("mongodb://127.0.0.1:27017");
+
+let db;
+client.connect().then(() => {
+    db = client.db("uol_chat_db");
+});
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -7,18 +17,18 @@ app.use(cors());
 
 const participants = [];
 
-app.post("/participants", (req, res) => {
-    const {name} = req.body;
-    if(!name || name==="") return res.status(422).send("O nome de usuário deve ser informado.");
-    const existParticipant = participants.find((participant) => participant.name == name);
-    if(existParticipant) return res.status(409).send("Este nome de usuário já existe.");
-    participants.push(
-        {
-            name: name,
-           // lastStatus: Date.now()
-        }
-    );
-    res.status(201).send("OK");
+app.post("/participants", async (req, res) => {
+    const user = req.body;
+    if(!user.name || user.name==="") return res.status(422).send("O nome de usuário deve ser informado.");
+    if(participants.find((participant) => participant.name == user.name)) return res.status(409).send("Este nome de usuário já existe.");
+    try{
+        await db.collection("participants").insertOne(user);
+        res.status(201).send("OK"); 
+    }
+    catch (error){
+        res.status(400).send("Error");
+    }
+
 });
 
 
