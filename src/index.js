ad0f92e990cf7568a10765dc449928253baf4546
LeonMesquita <leonmesquita16@gmail.com>
2022-06-23 12:12:28 -0300

    enviar mensagens

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import dayjs from 'dayjs';
+dayjs().format()
+
 import express from 'express';
 import cors from 'cors';
 import {MongoClient} from 'mongodb';
+
+
+console.log(dayjs().format("hh:mm:ss"));
 
 const client = new MongoClient("mongodb://127.0.0.1:27017");
 
@@ -18,7 +24,7 @@ app.use(cors());
 const participants = [];
 
 app.post("/participants", async (req, res) => {
-    const user = req.body;
+    const user = {...req.body, lastStatus: Date.now()};
     if(!user.name || user.name==="") return res.status(422).send("O nome de usuário deve ser informado.");
     if(participants.find((participant) => participant.name == user.name)) return res.status(409).send("Este nome de usuário já existe.");
     try{
@@ -32,5 +38,50 @@ app.post("/participants", async (req, res) => {
 });
 
 
+app.get("/participants", async (req, res) =>{
+    try{
+        const participantsList = await db.collection("participants").find().toArray();
+        res.send(participantsList);
+    } catch(error){
+        res.status(400).send("Error!");
+    }
+
+});
+
+app.post("/messages", async (req, res) => {
+    const message = {...req.body, from: req.headers.user, time: dayjs().format("hh:mm:ss")}
+    try{
+        await db.collection("messages").insertOne(message);
+         console.log(message);
+        res.status(201).send("OK");
+    } catch(error){
+        res.status(400).send(error);
+    }
+});
+
+
+app.get("/messages", async (req, res) => {
+    const limit = parseInt(req.query["limit"]);
+    let contMessages = 0;
+    const newMessagesList = [];
+    const user = req.headers.user;
+    try{
+        const messagesList = await db.collection("messages").find().toArray();
+        for(let count = messagesList.length-1; count >= 0; count--){
+            const message = messagesList[count];
+            if(message.type === "message" || message.from === user || message.to === user){
+                newMessagesList.push(message);
+                contMessages++;
+            }
+            if(contMessages === limit)
+                break;
+        }
+
+
+    } catch(error){
+
+    }
+
+});
 
 app.listen(5000, ()=> console.log("servidor rodando"));
